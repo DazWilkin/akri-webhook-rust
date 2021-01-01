9f3918c301b383490c2a8cae293b3e8bfb7b461f
DazWilkin <daz.wilkin@gmail.com>
2021-01-01 09:46:14 -0800

    Image by language tag

--- build.sh
+++ build.sh
@@ -3,9 +3,15 @@
 REGISTRY="ghcr.io"
 PREFIX="akri-webhook"
 REPO="${REGISTRY}/dazwilkin/${PREFIX}"
-TAG=$(git rev-parse HEAD)
+TAG="$(git rev-parse HEAD)"
 
 IMAGE=${REPO}:${TAG}
 
+# Create unique image for repo:tag
 docker build --tag=${IMAGE} --file=./Dockerfile .
 docker push ${IMAGE}
+
+# Create language identifying repo:lang
+LANG="rust"
+docker tag ${IMAGE} ${REPO}:${LANG}
+docker push ${REPO}:${LANG}
